d7f2be1a13d147e9c14dc5468421f8ad32db156b
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2020-08-09 20:49:52 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-import "./styles.css";
-
-function assert(value, desc) {
-  var li = document.createElement("li");
-  li.className = value ? "pass" : "fail";
-  li.appendChild(document.createTextNode(desc));
-  document.getElementById("results").appendChild(li);
-}
-
 var text = "かたじけない";
 function useless(callback) {
   return callback();
